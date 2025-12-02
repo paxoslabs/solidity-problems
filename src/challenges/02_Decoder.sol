24e9dfa92fe04f9ded698844619cd5e9deecb1c1
Jun Kim <64379343+junkim012@users.noreply.github.com>
2025-12-02 13:15:05 -0500

    feat: remove solution

--- src/challenges/02_Decoder.sol
+++ src/challenges/02_Decoder.sol
@@ -19,18 +19,6 @@ contract UniswapDecoder {
         virtual
         returns (bytes memory addressesFound)
     {
-        // Nothing to sanitize
-        // Return addresses found
-        // Determine how many addresses are in params.path.
-        uint256 chunkSize = 23; // 3 bytes for uint24 fee, and 20 bytes for address token
-        uint256 pathLength = params.path.length;
-        if (pathLength % chunkSize != 20) revert UniswapV3DecoderAndSanitizer__BadPathFormat();
-        uint256 pathAddressLength = 1 + (pathLength / chunkSize);
-        uint256 pathIndex;
-        for (uint256 i; i < pathAddressLength; ++i) {
-            addressesFound = abi.encodePacked(addressesFound, params.path[pathIndex:pathIndex + 20]);
-            pathIndex += chunkSize;
-        }
-        addressesFound = abi.encodePacked(addressesFound, params.recipient);
+        // Implement
     }   
 }
